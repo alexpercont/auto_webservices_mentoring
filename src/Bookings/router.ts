1840523b9ae233f3a6d65d00f94fe5818bd47ec4
Alex Percont <alexpercont@gmail.com>
2022-01-19 19:52:04 -0600

    Agregar validación cuando no hay información.

--- src/Bookings/router.ts
+++ src/Bookings/router.ts
@@ -10,9 +10,14 @@ router.get('/', function(req: Request, res: Response, next: NextFunction) {
 
 router.post('/', function(req: Request, res: Response, next: NextFunction){
     const datos = req.body
-    console.log(datos)
-    data.push(datos)
-    res.sendStatus(201)
+    if (Object.keys(datos).length){
+        console.log(datos)
+        data.push(datos)
+        res.sendStatus(201)
+    } else {
+        console.log("Request does not contain data");
+        res.sendStatus(400);
+    }
 })
 
 export = router;
