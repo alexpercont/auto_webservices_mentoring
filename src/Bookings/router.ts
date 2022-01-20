588485bca356bb93b44521d58c5029acc543a7bb
Alex Percont <alexpercont@gmail.com>
2022-01-19 18:58:04 -0600

    Agregar lineas de consola.

--- src/Bookings/router.ts
+++ src/Bookings/router.ts
@@ -4,11 +4,14 @@ import { Request, Response, NextFunction } from 'express';
 
 const router = express.Router();
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
+    console.log("Requesting all data.")
     res.json(data);
 })
 
 router.post('/', function(req: Request, res: Response, next: NextFunction){
-    data.push(req.body)
+    const datos = req.body
+    console.log(datos)
+    data.push(datos)
     res.sendStatus(201)
 })
 
